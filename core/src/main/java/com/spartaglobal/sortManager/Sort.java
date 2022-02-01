9d330c2fa21a754f0369269a95f6d545419a5207
Alvaro <63067669+Alvaropz@users.noreply.github.com>
2022-02-01 08:55:02 +0000

    Superclass Sort & Main Method
    This includes the superclass sort that will wrap the subclasses "Bubble" and "Merge" sort.
    Also, it's included the main method in the SortManager file.

--- core/src/main/java/com/spartaglobal/sortManager/Sort.java
+++ core/src/main/java/com/spartaglobal/sortManager/Sort.java
@@ -1,4 +1,14 @@
 package com.spartaglobal.sortManager;
 
-public class Sort {
+public abstract class Sort {
+    protected int[] arrayInts;
+    protected int arrayIntsLength;
+
+    public Sort(int[] arrayInts, int arrayIntsLength){
+        this.arrayInts = arrayInts;
+        this.arrayIntsLength = arrayIntsLength;
+    }
+
+    public abstract int[] sortArray();
 }
+
--- core/src/main/java/com/spartaglobal/sortManager/SortManager.java
+++ core/src/main/java/com/spartaglobal/sortManager/SortManager.java
@@ -1,4 +1,7 @@
 package com.spartaglobal.sortManager;
 
 public class SortManager {
+    public static void main(String[] args) {
+
+    }
 }
