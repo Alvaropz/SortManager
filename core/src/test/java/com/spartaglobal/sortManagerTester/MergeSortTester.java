c73f9f8e00f20e82614e414b074bf0923f4c8abc
Alvaro <63067669+Alvaropz@users.noreply.github.com>
2022-02-01 09:04:29 +0000

    Merge Sort Tester

--- core/src/test/java/com/spartaglobal/sortManagerTester/MergeSortTester.java
+++ core/src/test/java/com/spartaglobal/sortManagerTester/MergeSortTester.java
@@ -1,4 +1,32 @@
 package com.spartaglobal.sortManagerTester;
 
+import com.spartaglobal.sortManager.BubbleSort;
+import com.spartaglobal.sortManager.MergeSort;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
 public class MergeSortTester {
+
+    @Test
+    @DisplayName("This checks if an array is sorted via the Bubble Sort method")
+    public void testMergeSort(){
+        int[] inputArray = new int[40];
+        int testArrayLength = inputArray.length;
+
+        for (int i = 0; i < inputArray.length; i++) {
+            inputArray[i] = (int)Math.floor(Math.random()*(100)+1);
+        }
+
+        int[] inputArrayCopy = inputArray.clone();
+        Arrays.sort(inputArrayCopy);
+
+        MergeSort mergeSort = new MergeSort(inputArray, testArrayLength);
+        String expected = Arrays.toString(mergeSort.sortArray());
+        String actual = Arrays.toString(inputArrayCopy);
+        Assertions.assertEquals(expected, actual);
+    }
+
 }
